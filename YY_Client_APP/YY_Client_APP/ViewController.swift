5ff0e85cfbd31e5ea470d9a18d202658f4fd2eab
YY <yaroslav2112@mail.ru>
2019-06-30 23:50:32 +0300

    Создал первую версию проекта

---
+++ YY_Client_APP/YY_Client_APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YY_Client_APP
+//
+//  Created by Ярослав Ясинский on 30/06/2019.
+//  Copyright © 2019 Ярослав Ясинский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
