c6afc4ddeb1d71fe1461999a89f74ddb80c62ae7
YY <yaroslav2112@mail.ru>
2019-07-01 00:18:15 +0300

    Добавление в проект синглтона

---
+++ YY_Client_APP/YY_Client_APP/Models/Session.swift
@@ -0,0 +1,18 @@
+//
+//  Session.swift
+//  YY_Client_APP
+//
+//  Created by Ярослав Ясинский on 01/07/2019.
+//  Copyright © 2019 Ярослав Ясинский. All rights reserved.
+//
+
+import Foundation
+
+class Session {
+    
+    static let instance = Session()
+    private init() {}
+    
+    var token: String = ""
+    var userID: Int = 0
+}
